ad94bd480c4b70b6c8a3eeb1e7638f0c0b843f05
wins <win4709hi@gmail.com>
2021-04-29 15:38:09 +0800

    新增CheckList跟NumberList組件(解略架構而已)

---
+++ src/components/blocks/CheckoutList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="checkbox">
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckoutList',
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/blocks/NumberList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>1</p>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberList',
+};
+</script>
+
+<style lang="scss">
+</style>
